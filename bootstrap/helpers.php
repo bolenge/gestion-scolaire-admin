0851571b45d5233a67cf9b94691863290629b41a
Don de Dieu BOLENGE <dondedieubolenge@gmail.com>
2020-03-12 09:06:24 +0100

    Création de l'AuthAdminMiddleware

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -3,3 +3,27 @@
 	/****************************************************************\
 	 * CE FICHEIR EST RESERVE AUX FONTIONS A AJOUTER DANS LE PROJET *
 	\****************************************************************/
+
+	if (!function_exists('get_flash')) {
+		/**
+		 * Permet de renvoyer le message flash
+		 */
+		function get_flash() {
+			
+		}
+	}
+
+	if (!function_exists('set_flash')) {
+		/**
+		 * Permet de modifier ou enregistrer un message flash
+		 * @param string $type Le type de message flash
+		 * @param string $message Le message flash à sauver
+		 * @return void
+		 */
+		function set_flash(string $type, string $message) {
+			\session('flas', [
+				'type' => $type,
+				'message' => $message
+			]);
+		}
+	}
